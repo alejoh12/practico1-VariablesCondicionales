a5da98e6629083c62d83b50d46e0a6da49196346
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-18 22:10:08 -0300

    división de los dos números realizada.

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,8 +2,6 @@
 
 // Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
 
-// División de numero1 entre numero2.
-
 let numero1 = 10;
 let numero2 = 11;
 let resultado = null;
@@ -25,3 +23,8 @@ console.log("El resultado de la resta es: "+resultado);
 resultado = numero1 * numero2;
 
 console.log("El resultado de la multiplicación es: "+resultado);
+
+// División de numero1 entre numero2.
+resultado = numero1 / numero2;
+
+console.log("El resultado de la división es: "+resultado);
