97fab73eda6538dd3240a15930e1c655321e91d0
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-18 22:34:54 -0300

    cree dos variables y les asigné valores de string.

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,3 +1,6 @@
 // Ejercicio 2: Concatenación de Cadenas
 
 // Crea dos variables, nombre y apellido, y asígnales valores de tu elección. Luego, crea una tercera variable llamada nombreCompleto que contenga la concatenación de nombre y apellido. Muestra nombreCompleto en la consola.
+
+let nombre = "Facundo Alejo";
+let apellido = "Herrera";
