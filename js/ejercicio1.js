72cb761bf263a37eae8aa221703b2d4a90f770aa
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-18 22:08:39 -0300

    multiplicación de los dos números realizada.

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,7 +2,6 @@
 
 // Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
 
-// Multiplicación de numero1 y numero2.
 // División de numero1 entre numero2.
 
 let numero1 = 10;
@@ -20,3 +19,9 @@ console.log("El resultado de la suma es: "+resultado);
 resultado = numero1 - numero2;
 
 console.log("El resultado de la resta es: "+resultado);
+
+
+// Multiplicación de numero1 y numero2.
+resultado = numero1 * numero2;
+
+console.log("El resultado de la multiplicación es: "+resultado);
