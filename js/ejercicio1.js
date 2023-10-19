c78f32134f63b1876446d4df207db8bc3ef02795
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-18 22:05:49 -0300

    suma de los dos números realizada.

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,10 +2,16 @@
 
 // Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
 
-// Suma de numero1 y numero2.
 // Resta de numero1 y numero2.
 // Multiplicación de numero1 y numero2.
 // División de numero1 entre numero2.
 
 let numero1 = 10;
 let numero2 = 11;
+let resultado = null;
+
+// Suma de numero1 y numero2.
+
+resultado = numero1 + numero2;
+
+console.log("El resultado de la suma es: "+resultado);
