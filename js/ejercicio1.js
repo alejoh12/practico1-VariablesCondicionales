8b22b1d88d46057deeadcad4fd5691efa3ec8785
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-18 21:50:55 -0300

    estructura del proyecto completa

---
+++ js/ejercicio1.js
@@ -0,0 +1,9 @@
+// Ejercicio 1: Variables y Operaciones Matemáticas
+
+// Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
+
+// Suma de numero1 y numero2.
+// Resta de numero1 y numero2.
+// Multiplicación de numero1 y numero2.
+// División de numero1 entre numero2.
+
---
+++ js/ejercicio3.js
@@ -0,0 +1,4 @@
+// Ejercicio 3: Conversión de Grados Celsius a Fahrenheit
+
+// Crea una variable llamada gradosCelsius y asígnale un valor en grados Celsius. Luego, utiliza la fórmula de conversión para calcular los grados Fahrenheit y almacénalos en una variable llamada gradosFahrenheit. Muestra ambos valores en la consola.
+
