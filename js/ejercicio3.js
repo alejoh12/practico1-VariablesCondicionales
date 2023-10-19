6c142986157abf878bf3accbbc900ff2a1d68c4c
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-18 22:43:57 -0300

    cree una variable y le asigné un valor en grados celsius.

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -2,3 +2,4 @@
 
 // Crea una variable llamada gradosCelsius y asígnale un valor en grados Celsius. Luego, utiliza la fórmula de conversión para calcular los grados Fahrenheit y almacénalos en una variable llamada gradosFahrenheit. Muestra ambos valores en la consola.
 
+let gradosCelsius = 40;
