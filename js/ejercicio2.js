6b260de756234259be3f87640a46061acbc76619
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-23 19:07:59 -0300

    El ejercicio 2 terminado y corregido el 1.

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,1 +1,3 @@
 // 2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+
+document.write("<h1 style='color: blueviolet;'>&lt;&lt;Hello World&gt;&gt;</h1>");
