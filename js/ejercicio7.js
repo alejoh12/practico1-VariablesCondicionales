657ae1da98155416cb9a5ec3f42a9e4e2d62d365
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-23 19:44:51 -0300

    El ejercicio 7 terminado.

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -8,3 +8,19 @@ input: 15 , 3, 9
 Output: El 15 es el número más grande
 
 */
+
+let numero1 = parseInt(prompt("Ingresa un número por favor."));
+let numero2 = parseInt(prompt("Ingresa otro un número por favor."));
+let numero3 = parseInt(prompt("Ingresa otro un número diferente por favor."));
+
+if(numero1 > numero2 && numero1 > numero3) {
+    document.write("El "+numero1+" es el número más grande.");
+}else {
+    if(numero2 > numero1 && numero2 > numero3) {
+        document.write("El "+numero2+" es el número más grande.");
+    }else {
+        if(numero3 > numero1 && numero3 > numero2) {
+            document.write("El "+numero3+" es el número más grande.");
+        };
+    }
+}
