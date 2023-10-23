c53c0caaea0d3daa5d99d3c35379924d97f54596
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-23 19:18:20 -0300

    El ejercicio 5 terminado.

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -3,3 +3,7 @@
 Nota: Tener en cuenta la siguiente función: parseInt
 https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
 */
+
+let numero1 = parseInt(prompt("Ingresa un número por favor."));
+let numero2 = parseInt(prompt("Ingresa otro un número por favor."));
+document.write("La suma del número "+numero1+" más el número "+numero2+" es = "+(numero1+numero2));
