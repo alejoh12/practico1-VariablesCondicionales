dbd2b0bf873a5a8aa3c59b58f70c9ec7576d29c3
Alejo Herrera <alejoherrera1166@gmail.com>
2023-10-23 21:07:14 -0300

    El ejercicio 10 terminado.

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -8,3 +8,20 @@ input: 20
 Output: El 20 es divisible por 2.
 
 */
+let numero = parseInt(prompt("Ingrese un número, por favor."));
+
+if(numero % 2 === 0) {
+    document.write("El "+numero+" es divisible por 2.");
+}else {
+    if(numero % 3 === 0) {
+        document.write("El "+numero+" es divisible por 3.");
+    }else {
+        if(numero % 5 === 0) {
+            document.write("El "+numero+" es divisible por 5.");
+        }else {
+            if(numero % 7 === 0) {
+                document.write("El "+numero+" es divisible por 7.");
+            };
+        }
+    }
+}
